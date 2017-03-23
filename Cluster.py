b949f331dfad3b7e546125018956dce610a7f704
ShounakA <shounak.p.amladi@gmail.com>
2017-03-23 12:21:14 -0400

    Finished Point

--- Cluster.py
+++ Cluster.py
@@ -1,23 +1,29 @@
 import torch as t
 import numpy as np
+from Point import Point
 
 class Cluster:
-    def __init__(self, name):
-        self.centroid = 0
-        self.name = name
-        self.points = []
+    def __init__(self, name):#encapsulation
+        self.__centroid = 0
+        self.__name = name
+        self.__points = []
+
     def get_name(self):
-        return self.name
+        return self.__name
     def get_centroid(self):
-        return self.centroid
+        return self.__centroid
     def set_centroid(self, centre):
-        self.centroid = centre
+        self.__centroid = centre
         return
     def set_name(self, name):
-        self.name = name;
+        self.__name = name;
         return
     def to_string(self):
-        return "Name: " +str(self.name) +" |Centroid: "+ str(self.centroid)
+        return "Name: " +str(self.__name) +" | Centroid: "+ str(self.__centroid)
+
+    def calc_centroid(self):
+        return
+
 
 
 #self testing main
@@ -28,3 +34,5 @@ print(x.get_name())
 x.set_centroid(120)
 print(x.get_centroid())
 print(x.to_string())
+x = Point(1,2)
+print (x.get_x())
--- Point.py
+++ Point.py
@@ -1,2 +1,21 @@
 import torch as t
 import numpy as np
+class Point:
+    def __init__(self, x,y):#exncap
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def set_x(self, x):
+        self.__x= x
+        return
+    def set_y(self, y):
+        self.__y = y
+        return
+    def to_string(self):
+        return "X Value: " + str(__x) + " | Y Value: " + str(__y)
